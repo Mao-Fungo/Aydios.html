dba4be4cc02b39fb605824bad0cfd0459da0980f
Mao-Fungo <manuel.rodrigo2005@gmail.com>
2025-05-29 10:06:31 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,21 @@
+document.getElementById('carrera1').addEventListener('click', function() {
+            let car1 = document.getElementById ("car1");
+            car1.querySelector('p').textContent = "Aprende las técnicas esenciales de panadería y pastelería, desde la elaboración de panes artesanales hasta la creación de postres exquisitos.";
+        });
+            document.getElementById('carrera2').addEventListener('click', function() {
+            let car2 = document.getElementById ("car2");
+            car2.querySelector('p').textContent = "Domina las habilidades necesarias para brindar un servicio excepcional en la recepción de hoteles y establecimientos turísticos.";
+        });
+            document.getElementById('carrera3').addEventListener('click', function() {
+            let car3 = document.getElementById ("car3");
+            car3.querySelector('p').textContent = "Aprende a gestionar ventas al detalle, desde la atención al cliente hasta la gestión de inventarios en tiendas.";
+        });
+            document.getElementById('carrera4').addEventListener('click', function() {
+            let car4 = document.getElementById ("car4");
+            car4.querySelector('p').textContent = "Conviértete en un experto en computación, aprendiendo desde el manejo básico de software hasta la programación avanzada.";
+        });
+
+            document.getElementById('carrera5').addEventListener('click', function() {
+            let car5 = document.getElementById ("car5");
+            car5.querySelector('p').textContent = "Descubre el arte de la peluquería y la belleza personal, aprendiendo técnicas de corte, peinado y cuidado del cabello.";
+        });
